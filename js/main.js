fccf5c6fac93c0e5e8ec1dac573b3cdb93f717d8
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-14 12:08:37 -0500

    Add a preloader.gif

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-
-
-
-
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+    // makes sure that whole site is loaded
+    $('#preloader-gif, #preloader').fadeOut(5000, function () {});
+});
 
 $(function () {
     console.log('The DOM is ready to be used.');
